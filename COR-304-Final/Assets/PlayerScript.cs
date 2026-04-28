bc94f07e1ad0d478eb36cd7e7d85cd2536c91f26
AndreaLinnea <mueckeman@gmail.com>
2026-04-28 01:17:33 -0400

    audio

--- COR-304-Final/Assets/PlayerScript.cs
+++ COR-304-Final/Assets/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,17 +8,39 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] Camera playerCamera;
     [SerializeField] float sens = 100f;
     [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] AudioClip[] audioClips;
+    [SerializeField] AudioSource audioSource;
+    int audioIndex = 0;
+    bool isPlayingAudio = false;
 
     float xRot;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        StartCoroutine(changeAudio());
+    }
+
+
+    private IEnumerator changeAudio()
+    {
+        isPlayingAudio = true;
+       audioSource.PlayOneShot(audioClips[audioIndex],1f);
+        yield return new WaitForSeconds(audioClips[audioIndex].length);
+        isPlayingAudio = false;
+        audioIndex++;
+        audioIndex %= audioClips.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (!isPlayingAudio)
+        {
+            StartCoroutine(changeAudio());
+        }
+
         Debug.Log(InputSubscription.Instance.LookInput);
 
         float mouseX = InputSubscription.Instance.LookInput.x * sens * Time.deltaTime;
